e8c4a4154ca9dc28559ce80cb8a8df96ca5770cd
Alexandr Fyodorov <aleksandr.vy.fedorov@gazprombank.ru>
2025-05-20 13:40:13 +0300

    Создание сервиса

---
+++ src/test/java/com/example/fincoreaccountservice/FinCoreAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fincoreaccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinCoreAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
